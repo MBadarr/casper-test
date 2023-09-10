741f4dad661078fd760867849fb95d58cae3185c
M.Badar <mbadar418@gmail.com>
2023-09-11 03:19:14 +0500

    inital setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <div className='text-center'>
+        <h1 className='text-red-500'>sad</h1>
+      </div>
+    </>
+  )
+}
+
+export default App;

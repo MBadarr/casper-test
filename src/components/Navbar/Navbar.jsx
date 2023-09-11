ff6bbe6eb1f83b2dcecdaf4c9dfe9737fbd7a0ad
M.Badar <mbadar418@gmail.com>
2023-09-12 02:13:33 +0500

    navbar reponsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,12 +10,12 @@ function Navbar() {
     const imgURL = 'https://casper.com/on/demandware.static/Sites-casper_us-Site/-/default/dw0bfc8dd5/svg-icons/logo.svg';
     return (
 
-        <header className="bg-white shadow-sm">
-            <nav className="flex flex-wrap justify-between px-6 py-2 mx auto">
-                <div className="flex justify-center md:justify-center">
+        <header className="bg-white shadow-sm sticky top-0 z-10">
+            <nav className="flex justify-start flex-col md:flex-row lg:flex-row md:justify-between lg:jusify-between px-6 py-2 mx auto">
+                <div className="h-0 md:h-1/4 lg:h-1/4">
                     <img src={imgURL} alt="cart" />
                 </div>
-                <div className="flex flex-wrap justify-center items-center gap-10">
+                <div className="flex justify-start mt-14 md:mt-0 lg:mt-0 flex-col md:flex-row lg:flex-row md:justify-between md:items-center lg:items-center lg:jusify-between gap-6 md:gap-8 lg-gap-8">
                     {NavLinks.map((link) => (
                         <Link to={link.path} key={link.name}>
                             {link.name}
@@ -23,7 +23,7 @@ function Navbar() {
                     ))}
                 </div>
                 <div>
-                    <div className="flex items-center mt-2">
+                    <div className="hidden md:mt-2 lg:mt-2 md:block lg:block">
                         <img src={cart} alt="cart" height={24} width={26} />
                     </div>
                 </div>

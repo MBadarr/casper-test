5334ea2a4aa9d423c69e6577cacc614dedd135b6
M.Badar <mbadar418@gmail.com>
2023-09-12 01:30:52 +0500

    hero section

--- src/components/HerSection.jsx
+++ src/components/HerSection.jsx
@@ -1,7 +1,13 @@
 
 function HerSection() {
+    const imgURL = 'https://casper.com/dw/image/v2/BFHV_PRD/on/demandware.static/-/Library-Sites-casper-shared-us/default/dwce595458/images/Trial-Hero-Poster-Small-EU-Desktop%20(13).jpg?sw=2048&sm=cut'
     return (
-        <div>HerSection</div>
+        <div className="text-center text-header_text flex justify-center items-center">
+            <img src={imgURL} alt="hero" />
+            <div className="absolute">
+                <h1 className="text-2xl md:text-5xl lg:text-7xl font-medium">Sleep on it with a risk-free trial</h1>
+            </div>
+        </div>
     )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,23 +10,25 @@ function Navbar() {
     const imgURL = 'https://casper.com/on/demandware.static/Sites-casper_us-Site/-/default/dw0bfc8dd5/svg-icons/logo.svg';
     return (
 
-        <nav className="bg-white shadow-sm flex justify-between px-4 py-4">
-            <div>
-                <img src={imgURL} alt="cart" />
-            </div>
-            <div className="flex justify-center items-center gap-10">
-                {NavLinks.map((link) => (
-                    <Link to={link.path} key={link.name}>
-                        {link.name}
-                    </Link>
-                ))}
-            </div>
-            <div>
-                <div className="flex items-center mt-2">
-                    <img src={cart} alt="cart" height={24} width={26} />
+        <header className="bg-white shadow-sm">
+            <nav className="flex flex-wrap justify-between px-6 py-2 mx auto">
+                <div className="flex justify-center md:justify-center">
+                    <img src={imgURL} alt="cart" />
                 </div>
-            </div>
-        </nav >
+                <div className="flex flex-wrap justify-center items-center gap-10">
+                    {NavLinks.map((link) => (
+                        <Link to={link.path} key={link.name}>
+                            {link.name}
+                        </Link>
+                    ))}
+                </div>
+                <div>
+                    <div className="flex items-center mt-2">
+                        <img src={cart} alt="cart" height={24} width={26} />
+                    </div>
+                </div>
+            </nav>
+        </header >
     )
 }
 

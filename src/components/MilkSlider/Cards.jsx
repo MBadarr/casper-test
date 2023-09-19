d46e15f3b489daaa73c80a7280a5d64e37468119
M.Badar <mbadar418@gmail.com>
2023-09-19 17:02:05 +0500

    chevron hover

--- src/components/MilkSlider/Cards.jsx
+++ src/components/MilkSlider/Cards.jsx
@@ -15,24 +15,29 @@ function Cards() {
                 <p className="text-base leading-10">Iconic layer cakes with unfrosted sides, gooey pie, and fudgy Cake Truffles.</p>
             </header>
             <div className="custom-scrollbar relative px-6 py-6 overflow-x-scroll">
+                <div className="group">
+                    {/* Left Chevron */}
+                    <div className="absolute top-60 left-0 z-50 flex items-center justify-center bg-chevron_bg h-20 w-20 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                        <img src={LeftChevron} alt="left chevron" className="h-6 w-6" />
+                    </div>
 
-                {/* Chevrons */}
-                <div className="absolute top-60 left-0 z-50 flex items-center justify-center bg-chevron_bg h-20 w-20">
-                    <img src={LeftChevron} alt="left chevron" className="h-6 w-6" />
-                </div>
-                <div className="absolute top-60 right-0 z-50 flex items-center justify-center bg-chevron_bg h-20 w-20">
-                    <img src={RightChevron} alt="left chevron" className="h-6 w-6" />
-                </div>
+                    {/* Right Chevron */}
+                    <div className="absolute top-60 right-0 z-50 flex items-center justify-center bg-chevron_bg h-20 w-20 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                        <img src={RightChevron} alt="right chevron" className="h-6 w-6" />
+                    </div>
 
-                <div className="flex flex-nowrap gap-4 min-w-max">
-                    {CardsContent.map(item => (
-                        <CardsData
-                            key={item.id}
-                            imgURL={item.imgSrc}
-                            title={item.title}
-                            description={item.description}
-                        />
-                    ))}
+
+
+                    <div className="flex flex-nowrap gap-4 min-w-max">
+                        {CardsContent.map(item => (
+                            <CardsData
+                                key={item.id}
+                                imgURL={item.imgSrc}
+                                title={item.title}
+                                description={item.description}
+                            />
+                        ))}
+                    </div>
                 </div>
             </div>
         </>

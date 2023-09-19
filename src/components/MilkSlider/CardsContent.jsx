d703bda10cea6e67c056557aa2c7f5760a1826e7
M.Badar <mbadar418@gmail.com>
2023-09-19 17:40:24 +0500

    added images

--- src/components/MilkSlider/CardsContent.jsx
+++ src/components/MilkSlider/CardsContent.jsx
@@ -1,34 +1,39 @@
+import Cake from "../../assets/images/Cake.jpeg";
+import Pastry from "../../assets/images/Pastry.jpeg";
+import Cookie from "../../assets/images/Cookie.jpeg";
+import Box from "../../assets/images/Box.jpg";
+import Desert from "../../assets/images/Desert.jpeg";
 
 const CardsContent = [
     {
         "id": 1,
         "title": "THE BIRTHDAY CLASSIC",
         "description": "Tried and true b’day cake flavor fanatics, take note.",
-        "imgSrc": "https://milkbarstore.com/cdn/shop/products/BMBackgroundRemoval_0111_27_aec3891e-ea10-4d97-88f6-1187252ad2d3.png?format=pjpg&v=1680274631&width=600"
+        "imgSrc": Cake
     },
     {
         "id": 2,
         "title": "THE BIRTHDAY CLASSIC",
         "description": "Tried and true b’day cake flavor fanatics, take note.",
-        "imgSrc": "https://milkbarstore.com/cdn/shop/products/BMBackgroundRemoval_0111_27_aec3891e-ea10-4d97-88f6-1187252ad2d3.png?format=pjpg&v=1680274631&width=600"
+        "imgSrc": Pastry
     },
     {
         "id": 3,
         "title": "THE BIRTHDAY CLASSIC",
         "description": "Tried and true b’day cake flavor fanatics, take note.",
-        "imgSrc": "https://milkbarstore.com/cdn/shop/products/BMBackgroundRemoval_0111_27_aec3891e-ea10-4d97-88f6-1187252ad2d3.png?format=pjpg&v=1680274631&width=600"
+        "imgSrc": Cookie
     },
     {
         "id": 4,
         "title": "THE BIRTHDAY CLASSIC",
         "description": "Tried and true b’day cake flavor fanatics, take note.",
-        "imgSrc": "https://milkbarstore.com/cdn/shop/products/BMBackgroundRemoval_0111_27_aec3891e-ea10-4d97-88f6-1187252ad2d3.png?format=pjpg&v=1680274631&width=600"
+        "imgSrc": Box
     },
     {
         "id": 5,
         "title": "THE BIRTHDAY CLASSIC",
         "description": "Tried and true b’day cake flavor fanatics, take note.",
-        "imgSrc": "https://milkbarstore.com/cdn/shop/products/BMBackgroundRemoval_0111_27_aec3891e-ea10-4d97-88f6-1187252ad2d3.png?format=pjpg&v=1680274631&width=600"
+        "imgSrc": Desert
     },
     {
         "id": 6,
